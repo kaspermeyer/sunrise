823b6374b48ac939389177242d00af3158837dd9
Kasper Meyer <hi@kaspermeyer.com>
2019-06-28 16:51:30 +0200

    Expand week 1 tests with properly formatted HTML

--- test/weekly/week_1_test.rb
+++ test/weekly/week_1_test.rb
@@ -4,44 +4,68 @@ class Week1Test < Sunrise::TestCase
   include Sunrise::Helper
 
   test "empty element" do
+    expected_output = <<~HTML
+      <div></div>
+    HTML
+
     output = html do
       div
     end
 
-    assert_equal '<div></div>', output
+    assert_equal squish_html_string(expected_output), output
   end
 
   test "element with text content" do
+    expected_output = <<~HTML
+      <div>Cool text</div>
+    HTML
+
     output = html do
       div do
         text 'Cool text'
       end
     end
 
-    assert_equal '<div>Cool text</div>', output
+    assert_equal squish_html_string(expected_output), output
   end
 
   test "element with attributes" do
+    expected_output = <<~HTML
+      <a href="http://google.com" class="underline bold">
+        Cool link!
+      </a>
+    HTML
+
     output = html do
       a href: 'http://google.com', class: 'underline bold' do
         text 'Cool link!'
       end
     end
 
-    assert_equal '<a href="http://google.com" class="underline bold">Cool link!</a>', output
+    assert_equal squish_html_string(expected_output), output
   end
 
   test "nested elements" do
+    expected_output = <<~HTML
+      <div>
+        <span>
+          <a href="http://google.com">
+            A link to google
+          </a>
+        </span>
+      </div>
+    HTML
+
     output = html do
       div do
         span do
           a href: 'http://google.com' do
-            text 'a link to google'
+            text 'A link to google'
           end
         end
       end
     end
 
-    assert_equal '<div><span><a href="http://google.com">a link to google</a></span></div>', output
+    assert_equal squish_html_string(expected_output), output
   end
 end
