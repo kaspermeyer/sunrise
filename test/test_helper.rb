a6d0605f8756c0a89f169df72872de3a29cb9bff
Kasper Meyer <hi@kaspermeyer.com>
2019-06-28 10:15:42 +0200

    Add Minitest Reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "sunrise"
 require "minitest/autorun"
+require "minitest/reporters"
 require "support/test_case"
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
