9f8f49a9de1cd3933c994bd7f4d43a158768c7ad
Kasper Meyer <hi@kaspermeyer.com>
2019-06-28 09:28:56 +0200

    Remove dummy tests

--- test/sunrise_test.rb
+++ test/sunrise_test.rb
@@ -1,11 +1,4 @@
 require "test_helper"
 
 class SunriseTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Sunrise::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
