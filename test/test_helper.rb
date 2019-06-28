36a164f115f3b15eb4d7d167d061d947dd5142ee
Kasper Meyer <hi@kaspermeyer.com>
2019-06-28 16:26:14 +0200

    Add test helper `#squish_html_string`

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,9 @@ require "minitest/reporters"
 require "support/test_case"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+# Converts multi-line HTML-string with arbitrary indentation to a squished one-line version
+def squish_html_string html_string
+  # Delete newlines and occurences of two or more whitespaces
+  html_string.delete("\n").gsub(/[ ]{2,}/, "")
+end
