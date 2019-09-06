85f3468dad9478b1e19257b3da0eec438ed06ef2
Peter Klogborg <klogborg@traels.it>
2019-09-06 16:54:47 +0200

    Blob. plob

--- lib/sunrise.rb
+++ lib/sunrise.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "sunrise/version"
+require "sunrise/html_block"
 require "sunrise/helper"
 
 module Sunrise
--- lib/sunrise/helper.rb
+++ lib/sunrise/helper.rb
@@ -2,8 +2,30 @@
 
 module Sunrise
   module Helper
-    def html &block
-      # Implement me!
+    def html(&block)
+      instance_exec(&block)
     end
+    %i[a].each do |block_word|
+      define_method(block_word) do |arg, &block|
+        return "<#{block_word} #{hash_html(arg)}>#{block.nil? ? '' : block.call}</#{block_word}>"
+      end
+    end
+    %i[div span].each do |block_word|
+      define_method(block_word) do |&block|
+        return "<#{block_word}>#{block.nil? ? '' : block.call}</#{block_word}>"
+      end
+    end
+
+    %i[text].each do |function_word|
+      define_method(function_word) do |arg|
+        return arg.to_s
+      end
+    end
+
+    private
+
+      def hash_html(hash)
+        hash.map { |key, value| (key.to_s + '="' + value.to_s + '"') }.join(" ")
+      end
   end
 end
